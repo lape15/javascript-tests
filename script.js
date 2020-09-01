eb54cca3166a303dc09397e6640da0e5b63de298
Lape15 <larape1993@gmail.com>
2020-09-01 08:56:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const websites  = [
+    'cats.com',
+    'souprecipes.com',
+    'flowers.com',
+    'animals.com',
+    'catpictures.com',
+    'myfavoritecats.com',
+
+]
+
+ const googleSearch = (searchInput,db) => {
+   const matches =  db.filter(website => {
+        return website.includes(searchInput)
+    })
+   return  matches.length > 3 ? matches.slice(0,3) : matches
+}
+module.exports = {
+    googleSearch
+}
---
+++ script.test.js
@@ -0,0 +1,22 @@
+const googleSearch = require('./script')
+
+let dbMock = [
+    'dog.com',
+    'cheese.com',
+    'disney.com',
+    'dogpictures.com'
+]
+
+describe('googlesearch tests', () => {
+    it('this is a test', () => {
+        expect('hello').toBe('hello')
+    })
+    
+    it('it is searching google', () => {
+        expect(googleSearch('dog',dbMock)).toEqual(['dog.com','dogpictures.com'])
+    })
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com',dbMock).length).toEqual(3)
+    })
+    
+})
